52b87c4b04d0d472cf463ec0f819d3b540963ec9
Pierrebthlr <bathellierp@gmail.com>
2017-11-10 14:36:20 +0100

    ajout dépôt objet
    ça marche pas encore

--- Assets/Scripts/RayCastingController.cs
+++ Assets/Scripts/RayCastingController.cs
@@ -9,6 +9,8 @@ public class RayCastingController : MonoBehaviour
 	private float distanceToObj;	// Distance entre le personnage et l'objet saisi
 	private Rigidbody attachedObject;	// Objet saisi, null si aucun objet saisi
 	private Vector3 objectSizeInitial;
+	private Vector3 secondObjectSize;
+
 	private float distanceInitial;
 	private float newSizeY;
 
@@ -31,30 +33,35 @@ public class RayCastingController : MonoBehaviour
 	void Update () 
 	{
 		// Le raycast attache un objet cliqué
-		RaycastHit hitInfo;
+		RaycastHit[] hitInfo;
+		RaycastHit firstHit;
 		Ray ray = GetComponentInChildren<Camera>().ScreenPointToRay(Input.mousePosition);
 		Debug.DrawRay (ray.origin, ray.direction * RAYCASTLENGTH, Color.blue);
-		bool rayCasted = Physics.Raycast (ray, out hitInfo, RAYCASTLENGTH);
+		bool rayCasted = Physics.Raycast (ray, out firstHit, RAYCASTLENGTH);
+		RaycastHit objectFirstPlane;
 
 		if (rayCasted) 
 		{
-			rayCasted = hitInfo.transform.CompareTag ("draggable");
+			rayCasted = firstHit.transform.CompareTag ("draggable");
+
 		}
 
-		if (Input.GetMouseButtonDown (0))	// L'utilisateur vient de cliquer
+		if (Input.GetMouseButtonDown (0) && attachedObject == null)	// L'utilisateur vient de cliquer
 		{
+
 			if (rayCasted) 
 			{
+				objectFirstPlane = firstHit;
 				Debug.Log ("Object attached");
-				attachedObject = hitInfo.rigidbody;
+				attachedObject = objectFirstPlane.rigidbody;
 				attachedObject.isKinematic = true;
-				distanceToObj = hitInfo.distance;
+				distanceToObj = objectFirstPlane.distance;
 
 				/** On place l'objet dans la main de l'utilisateur **/
 				//Déterminer la taille et la distance
 				objectSizeInitial = attachedObject.GetComponent<Renderer>().bounds.size;
 				float sizeY = objectSizeInitial.y;
-				distanceInitial = hitInfo.distance;
+				distanceInitial = objectFirstPlane.distance;
 				//Calculer nouvelle taille
 				newSizeY = BASEDISTANCE/distanceInitial*sizeY;
 				Debug.Log (sizeY);
@@ -62,13 +69,55 @@ public class RayCastingController : MonoBehaviour
 				float ratio = newSizeY / sizeY;
 				//attachedObject.GetComponent<Renderer>().bounds.size.Set(objectSizeInitial.x * ratio, newSizeY, objectSizeInitial.z * ratio);
 				attachedObject.transform.localScale = new Vector3(objectSizeInitial.x * ratio, newSizeY, objectSizeInitial.z * ratio);
-				//Translater
 				attachedObject.MovePosition (ray.origin + (ray.direction * BASEDISTANCE));
 			}
 		} 
 
-		else if (Input.GetMouseButtonUp (0) && attachedObject != null) 	// L'utilisateur relache un objet saisi
+		else if (Input.GetMouseButtonDown (0) && attachedObject != null) 	// L'utilisateur relache un objet saisi
 		{
+			hitInfo = Physics.RaycastAll (ray, (float)RAYCASTLENGTH);
+			Debug.Log (hitInfo.Length);
+			if (hitInfo.Length >= 2) {
+				RaycastHit objectSecondPlane;
+
+				objectFirstPlane =  hitInfo [0];
+				objectSecondPlane = hitInfo [1];
+				Debug.Log ("distance actuelle");
+				Debug.Log (hitInfo [0].distance);
+				Debug.Log ("nouvelle distance");
+				Debug.Log (hitInfo [1].distance);
+				Debug.Log ("info premier objet");
+				Debug.Log ( hitInfo [0].transform.name);
+				Debug.Log ( hitInfo [0].transform.position);
+
+				Debug.Log ("info deuxieme objet");
+				Debug.Log ( hitInfo [1].transform.name);
+				Debug.Log ( hitInfo [1].transform.position);
+
+
+				distanceToObj = objectSecondPlane.distance;
+				objectSizeInitial = attachedObject.GetComponent<Renderer>().bounds.size;
+				secondObjectSize = objectSecondPlane.transform.GetComponent<Renderer> ().bounds.size;
+				float sizeY = objectSizeInitial.y;
+				distanceInitial = objectFirstPlane.distance;
+				//Calculer nouvelle taille
+				newSizeY = (distanceToObj-secondObjectSize.x)/BASEDISTANCE*sizeY;
+				Debug.Log ("tailleactuelle");
+				Debug.Log (sizeY);
+				Debug.Log ("nouvelle taille");
+				Debug.Log (newSizeY);
+				float ratio = newSizeY / sizeY;
+				//Translater
+				Debug.Log ("ray direction");
+				Debug.Log (ray.direction);
+				Debug.Log ("nouvelle position");
+				Debug.Log (ray.origin + (ray.direction * (distanceToObj-(objectSizeInitial.x * ratio)/2)));
+
+				attachedObject.MovePosition (ray.origin + (ray.direction * (distanceToObj-(objectSizeInitial.x * ratio)/2)));
+				//attachedObject.GetComponent<Renderer>().bounds.size.Set(objectSizeInitial.x * ratio, newSizeY, objectSizeInitial.z * ratio);
+				attachedObject.transform.localScale = new Vector3(objectSizeInitial.x * ratio, newSizeY, objectSizeInitial.z * ratio);
+
+			
 			attachedObject.isKinematic = false;
 			attachedObject = null;
 			Debug.Log ("Object detached");
@@ -79,9 +128,11 @@ public class RayCastingController : MonoBehaviour
 			{
 				Cursor.SetCursor (cursorOff, hotSpot, cursorMode);
 			}
-		} 
+			
+		}
+		}
 
-		if (Input.GetMouseButton (0) && attachedObject != null) // L'utilisateur continue la saisie d'un objet
+		if ( attachedObject != null) // L'utilisateur continue la saisie d'un objet
 		{
 			attachedObject.MovePosition (ray.origin + (ray.direction * BASEDISTANCE));
 			Cursor.SetCursor (cursorDragged, hotSpot, cursorMode);
