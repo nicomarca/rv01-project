61125068fcdf5f691c37a2e9dd11777037bd4b96
Unknown <nicolas.marcadet@gmail.com>
2017-12-16 18:20:56 +0100

    scénario presque fini

--- Assets/Scripts/JailScenarioScript.cs
+++ Assets/Scripts/JailScenarioScript.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VR;
 
 public class JailScenarioScript : MonoBehaviour {
 
 	public Light light;
-	public GameObject wand;
+	public GameObject displayedWand;
+	public GameObject playerWand;
+	public GameObject stick;
 	public GameObject player;
 
 	private float timer;
 	private bool flashDone = false;
 	private bool flashStabilized = false;
+	private bool wandTaken = false;
 
 	void Start () {
 		timer = 0.0f;
@@ -19,12 +23,32 @@ public class JailScenarioScript : MonoBehaviour {
 	void Update () {
 		timer += Time.deltaTime;
 
+		flashLight ();
+
+		if (!wandTaken){
+			if (Input.GetMouseButtonDown (0) || Input.GetButtonDown ("Grab")){
+				Debug.Log (Vector3.Distance (displayedWand.transform.position, stick.transform.position));
+				if (Vector3.Distance (displayedWand.transform.position, stick.transform.position) < 2.5f) {
+					displayedWand.SetActive (false);
+					stick.SetActive (false);
+
+					player.GetComponent<RayCastingController> ().enabled = true;
+					playerWand.SetActive (true);
+					wandTaken = true;
+
+					light.intensity = 0;
+				}
+			}
+		}
+	}
+
+	void flashLight(){
 		if (timer > 2.0) {
 			if (!flashDone) {
 				light.intensity += 3.0f;
 				if (light.intensity > 12) {
 					flashDone = true;
-					wand.SetActive (true);
+					displayedWand.SetActive (true);
 					player.GetComponent<Rigidbody> ().constraints = RigidbodyConstraints.None;
 					player.GetComponent<Rigidbody> ().constraints = RigidbodyConstraints.FreezePositionY;
 				}
