f9623e4a7eead13008a43678bd14f0f13464339e
Unknown <nicolas.marcadet@gmail.com>
2017-12-16 15:53:02 +0100

    changement FPS

--- Assets/Scripts/FPSdeplacement.cs
+++ Assets/Scripts/FPSdeplacement.cs
@@ -25,20 +25,20 @@ public class FPSdeplacement : MonoBehaviour {
 			float mouvmentHorizontal = Input.GetAxis ("Horizontal");
 			float mouvmentVertical = Input.GetAxis ("Vertical");
 			Vector3 mouvment = new Vector3 (mouvmentHorizontal, 0, mouvmentVertical);
-			this.transform.Translate (Camera.main.transform.rotation * mouvment * tSpeed);
+			//this.transform.Translate (Camera.main.transform.rotation * mouvment * tSpeed);
+			transform.GetComponent<Rigidbody>().MovePosition(transform.position + Camera.main.transform.rotation*mouvment * tSpeed);
 		} else {
 			if (Input.GetKey ("z")) {
-				transform.
-				transform.Translate (Vector3.forward * tSpeed);
+				transform.GetComponent<Rigidbody>().MovePosition(transform.position + Camera.main.transform.rotation*Vector3.forward * tSpeed);
 			}
 			if (Input.GetKey ("s")) {
-				transform.Translate (Vector3.back * tSpeed);
+				transform.GetComponent<Rigidbody>().MovePosition(transform.position + Camera.main.transform.rotation*Vector3.back * tSpeed);
 			}
 			if (Input.GetKey ("q")) {
-				transform.Translate (Vector3.left * tSpeed);
+				transform.GetComponent<Rigidbody>().MovePosition(transform.position + Camera.main.transform.rotation*Vector3.left * tSpeed);
 			}
 			if (Input.GetKey ("d")) {
-				transform.Translate (Vector3.right * tSpeed);
+				transform.GetComponent<Rigidbody>().MovePosition(transform.position + Camera.main.transform.rotation*Vector3.right * tSpeed);
 			}
 
 			if (Input.GetKey ("left")) {
